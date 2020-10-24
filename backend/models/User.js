e195e30e7c1e170b0132c6e90e953e4d83874fe9
nnova-code <egharris14@gmail.com>
2020-10-23 17:49:56 -0700

    set up passport

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,23 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserScheme = new Schema({
+// Create Schema
+const UserSchema = new Schema({
   name: {
     type: String,
-    required: true,
+    required: true
   },
   email: {
     type: String,
-    required: true,
+    required: true
   },
   password: {
     type: String,
-    required: true,
+    required: true
   },
   date: {
     type: Date,
-    default: Date.now,
-  },
+    default: Date.now
+  }
 });
 
-module.exports = User = mongoose.model(("users", UserSchema));
+module.exports = User = mongoose.model("users", UserSchema);
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./backend/routes/api/users")
+
 const app = express();
+
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -9,8 +14,10 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
 // DB Config
 const db = require("./backend/config/keys").mongoURI;
+
 // Connect to MongoDB
 mongoose
   .connect(db, {
@@ -21,7 +28,18 @@ mongoose
   })
   .then(() => console.log("MongoDB successfully connected"))
   .catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./backend/config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+
 app.listen(port, () =>
   console.log(
     `Is your server running on port ${port}? Well then you better go catch it! LOLOL`
