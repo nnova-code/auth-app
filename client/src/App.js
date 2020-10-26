17772dc7364e2d69ffd519d41b3f3e9a57d3dfc1
nnova-code <egharris14@gmail.com>
2020-10-26 15:04:57 -0700

    format code

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -10,12 +10,12 @@ import Login from "./components/auth/Login";
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/login" component={Login} />
-    </div>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/resgister" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </div>
     </Router>
   );
 }
