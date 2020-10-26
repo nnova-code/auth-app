5a56bdc665cd56e69318b67843850e0394d8adcb
nnova-code <egharris14@gmail.com>
2020-10-26 14:59:40 -0700

    create login and register pages and route in app.js

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,117 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+function Register () {
+
+  const [form, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+    errors: {}
+  });
+
+  function onChange (e){
+    const {value, id} = e.target;
+    setValues({
+      ...form,
+      [id]: value
+    });
+  }
+
+  function onSubmit (e) {
+    e.preventDefault();
+
+    const {name, email, password, password2} = form;
+
+    const newUser = {
+      name,
+      email,
+      password,
+      password2,
+    };
+
+    console.log(newUser);
+  };
+
+  const { name, email, password, password2, errors } = form;
+
+  return (
+    <div className="container">
+        <div className="row">
+          <div className="col s8 offset-s2">
+            <Link to="/" className="btn-flat waves-effect">
+              <i className="material-icons left">keyboard_backspace</i> Back to
+              home
+            </Link>
+            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+              <h4>
+                <b>Register</b> below
+              </h4>
+              <p className="grey-text text-darken-1">
+                Already have an account? <Link to="/login">Log in</Link>
+              </p>
+            </div>
+            <form noValidate onSubmit={onSubmit}>
+              <div className="input-field col s12">
+                <input
+                  onChange={onChange}
+                  value={name}
+                  error={errors.name}
+                  id="name"
+                  type="text"
+                />
+                <label htmlFor="name">Name</label>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  onChange={onChange}
+                  value={email}
+                  error={errors.email}
+                  id="email"
+                  type="email"
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  onChange={onChange}
+                  value={password}
+                  error={errors.password}
+                  id="password"
+                  type="password"
+                />
+                <label htmlFor="password">Password</label>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  onChange={onChange}
+                  value={password2}
+                  error={errors.password2}
+                  id="password2"
+                  type="password"
+                />
+                <label htmlFor="password2">Confirm Password</label>
+              </div>
+              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                <button
+                  style={{
+                    width: "150px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                    marginTop: "1rem"
+                  }}
+                  type="submit"
+                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                >
+                  Sign up
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Register ;
