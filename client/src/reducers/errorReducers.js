c8190cc1f6c13e3a9e7465e6904e6ef1194b7624
nnova-code <egharris14@gmail.com>
2020-10-26 15:33:56 -0700

    set up auth token and format

--- client/src/reducers/errorReducers.js
+++ client/src/reducers/errorReducers.js
@@ -2,7 +2,7 @@ import { GET_ERRORS } from "../actions/types";
 
 const initialState = {};
 
-function errorReducer (state = initialState, action) {
+function errorReducer(state = initialState, action) {
   switch (action.type) {
     case GET_ERRORS:
       return action.payload;
